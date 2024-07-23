5e78b7f91f6cac7e41985077fce1f93656987437
aspirelaptop <aceraspire8246@gmail.com>
2024-07-24 00:11:08 +0500

    Funksiyagacha bo'lgan qismi bilan ishlandi

--- firstapp.php
+++ firstapp.php
@@ -1,4 +1,23 @@
 <?php
 
+    class Avtomobile {
+
+        public $model;
+        public $rang;
+
+        public function __construct($model, $rang) {
+            $this->model = $model;
+            $this->rang = $rang;
+        }
+
+
+        public function index() {
+            return "Mashina markasi: $this->model va rangi: $this->rang";
+        }
+    }
+
+
+    $obj = new Avtomobile("Tracker", "Qora");
+    echo $obj->index();
 
 ?>
